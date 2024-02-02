4c533dd8cb8ed1dc62c70a508a9685c616f704c5
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-02 19:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 	let fName = document.getElementById("fname");
 	let lName = document.getElementById("lname");
-	alert(fName.value, lName.value)
+	alert(fName.value+" "+lName.value);
 }
 
